9d043eb49473b939a40491f8bb84c983c05fd0ff
Paula Schwetzler <paulaschwetzler@gmx.de>
2021-03-20 14:13:23 +0100

    Enter

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ Hi Vatro,
 so this is my first attempt to build a python code, which can read and count the different letters.
 
 Bye, Paula
+
+- Aufgaben entwickeln
+- asd
+- asdajsidn
+* asjdansd
+# asdaasfoafsjo
--- Test.py
+++ Test.py
@@ -1,1 +1,12 @@
-print("Hello world")
+import string
+def readfile(filename):
+    with open(filename, 'r') as f:
+        data = f.read()
+    return data
+
+def filterWords(inputText):
+    filterresult = ''
+    filterresult.join([x for x in inputText if x in string.ascii_letters + '\'- '])
+    return filterresult
+
+print(filterWords(readfile("README.md")))
