6de658b6d9667f33cfccbc3a3c1176dcf52c5c45
Your Name <you@example.com>
2021-03-21 09:43:55 +0100

    boring sunday morning ...

--- Main.py
+++ Main.py
@@ -3,5 +3,7 @@ import re
 import WordsLogic
 import ReadWriteOperations
 
-fileName = "README.md"
-print(WordsLogic.filterWords(ReadWriteOperations.readFile(fileName)))
+fileName = "writefile.txt"
+text = "super cool random text"
+ReadWriteOperations.writeToFile("writefile.txt",text, False)
+print(WordsLogic.filterWords(ReadWriteOperations.readFromFile(fileName)))
--- ReadWriteOperations.py
+++ ReadWriteOperations.py
@@ -1,9 +1,12 @@
-def readFile(filename):
+def readFromFile(filename):
     with open(filename, 'r') as f:
         data = f.read()
     return data
 
-def writeFile(filename):
-    with open(filename, 'r') as f:
-        data = f.read()
-    return data
+# function to write content to file. by default we append content to existing file
+def writeToFile(filename, value, append=True):
+    #if we pass value false to function then operation is 'w' meaning we delete existing content
+    operation = 'a' if append else 'w' 
+    f = open(filename, operation )
+    f.write(value)
+    f.close()
